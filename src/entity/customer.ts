a841809dafb797a125bd2f1a6b54e9384bce99cc
Jonatas Alves <jonatasalves@id.uff.br>
2024-09-19 11:34:10 -0300

    Rewards

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -5,11 +5,24 @@ export default class Customer {
   private _name: string = ""
   private _address?: Address
   private _active: boolean = false
+  private _rewardPoints: number = 0
 
   constructor(id: string, name: string) {
     this._id = id
     this._name = name
     this.validate()
+  }
+
+  get id(): string {
+    return this._id
+  }
+
+  get name(): string {
+    return this._name
+  }
+
+  get rewardPoints(): number {
+    return this._rewardPoints
   }
 
   validate() {
@@ -19,10 +32,6 @@ export default class Customer {
     if (this._name.length === 0) {
       throw new Error("Name is required")
     }
-  }
-
-  get name(): string {
-    return this._name
   }
 
   changeName(name: string) {
@@ -43,6 +52,10 @@ export default class Customer {
 
   deactivate() {
     this._active = false
+  }
+
+  addRewardPoints(points: number) {
+    this._rewardPoints += points
   }
 
   set Address(address: Address) {
