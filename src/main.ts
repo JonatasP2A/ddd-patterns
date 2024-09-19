e11ceb1034a6e064b4faae266b480469c3ef6071
Jonatas Alves <jonatasalves@id.uff.br>
2024-09-19 10:32:05 -0300

    catch type error on test

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ const address = new Address("R. default", 333, "12345-123", "City");
 customer.Address = address;
 customer.activate();
 
-const item1 = new OrderItem("1", "Item 1", 100);
-const item2 = new OrderItem("2", "Item 2", 200);
+const item1 = new OrderItem("1", "Item 1", 100, "p1", 1);
+const item2 = new OrderItem("2", "Item 2", 200, "p2", 1);
 const order = new Order("1", "123", [item1, item2]);
