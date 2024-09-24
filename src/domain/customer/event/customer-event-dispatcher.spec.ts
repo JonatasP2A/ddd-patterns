ac504517c42258d98c3c5aaa801c75eb0e7009c1
Jonatas Alves <jonatasalves@id.uff.br>
2024-09-24 06:47:23 -0300

    modularizing the domain layer

--- src/domain/customer/event/customer-event-dispatcher.spec.ts
+++ src/domain/customer/event/customer-event-dispatcher.spec.ts
@@ -1,5 +1,5 @@
-import Address from "../../entity/address";
-import EventDispatcher from "../@shared/event-dispatcher";
+import EventDispatcher from "../../@shared/event/event-dispatcher";
+import Address from "../value-object/address";
 import CustomerAddressChangedEvent from "./customer-address-changed.event";
 import CustomerCreatedEvent from "./customer-created.event";
 import NotifyOngoingDeliveryOperationsToRerouteHandler from "./handler/notify-ongoing-delivery-operations-to-reroute.handler";
